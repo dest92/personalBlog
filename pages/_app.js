8454760636d3675805be7ac3032e73492a9c63ad
dest92 <acebalmatias2019@gmail.com>
2023-04-05 09:31:53 -0300

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "../styles/global.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import Head from "next/head";
 
 library.add(fas);
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps}> </Component>
     </>
   );
